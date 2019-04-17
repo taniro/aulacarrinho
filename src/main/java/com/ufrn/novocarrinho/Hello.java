1e8d75ca014537e6d3bffbe49936780056c875f4
tanirocr <tanirocr@gmail.com>
2019-04-17 10:56:11 -0300

    inicial

---
+++ src/main/java/com/ufrn/novocarrinho/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ufrn.novocarrinho;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author tanir
+ */
+@ManagedBean
+public class Hello {
+    
+    public String sayHello(){
+        return "Hello from JSF";
+    }
+    
+}
